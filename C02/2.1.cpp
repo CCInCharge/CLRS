d41e048858f000fc924bdf05980a22cd37ed543d
CCInCharge <charles.l.chen.clc@gmail.com>
2016-11-30 01:13:03 -0800

    Added C++ implementation of insertion sort

---
+++ C02/2.1.cpp
@@ -0,0 +1,37 @@
+/* Chapter 2.1 - Insertion Sort */
+// Requires compilation with C++11
+
+#include <iostream>
+#include <vector>
+
+std::vector<double> insertionSort(std::vector<double> a)
+{
+    double temp;
+    for (int j = 0; j < a.size(); j++)
+    {
+        for (int i = j - 1; i >= 0; i--)
+        {
+            if (a.at(i + 1) <= a.at(i))
+            {
+                temp = a.at(i + 1);
+                a[i + 1] = a.at(i);
+                a[i] = temp;
+            }
+        }
+    }
+    return a;
+}
+/*
+int main()
+{
+    std::vector<double> b(8);
+    b = {7, 2, 3, 1, 9, 3, 1, 0};
+    std::vector<double> c(8);
+    c = insertionSort(b);
+    for (int i = 0; i < c.size(); i++)
+    {
+        std::cout << c.at(i) << std::endl;
+    }
+    return 0;
+}
+*/
